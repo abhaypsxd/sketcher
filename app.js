26ca0593fffacbc1d0a9045844ae319671552928
abhaypsxd <theabhay626@gmail.com>
2024-12-24 02:01:48 +0530

    add grid v2

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const container=document.querySelector(".container");
 for(let i = 0; i<256; i++){
     const square = document.createElement("div");
+    square.style.height="57px";
+    square.style.height="57px";
     container.appendChild(square);
 }
