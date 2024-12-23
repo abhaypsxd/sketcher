6d2040e5df27b9b7f7761ad2a245d72f998238af
abhaypsxd <theabhay626@gmail.com>
2024-12-24 02:27:43 +0530

    add hover trail effect

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const container=document.querySelector(".container");
 for(let i = 0; i<256; i++){
     const square = document.createElement("div");
-    square.style.height="57px";
-    square.style.height="57px";
+    square.style.width="58px";
+    square.style.height="58px";
+    square.id="square_div";
     container.appendChild(square);
 }
+const squares = document.querySelectorAll("#square_div");
+squares.forEach((square)=>{
+    square.addEventListener('mouseover',()=>{
+        square.style.backgroundColor='cyan';
+    })
+})
