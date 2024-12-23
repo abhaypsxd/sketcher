e4ac2f5fde7db2843a958641324f7124cda24992
abhaypsxd <theabhay626@gmail.com>
2024-12-24 03:06:40 +0530

    added randomized colors, and change pixel button.

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const container=document.querySelector(".container");
-for(let i = 0; i<256; i++){
+
+let colorLiterals = ["0","1","2","3","4","5","6","7","8","9","a","b","c,","d","e","f"];
+function colorConstructor(colorLiterals) {
+    let color="#";
+    for(let i = 0; i<6; i++){
+        let idx = Math.floor(Math.random()*colorLiterals.length);
+        color+=colorLiterals[idx];
+    }
+    return color;
+}
+// console.log(colorConstructor(colorLiterals))
+let nums = prompt("Enter the number of pixels:-")
+if(nums<100)
+for(let i = 0; i<nums*nums; i++){
+    let numStr = (960/nums-2).toString();
+    numStr=numStr+"px";
     const square = document.createElement("div");
-    square.style.width="58px";
-    square.style.height="58px";
+    square.style.width=numStr;
+    square.style.height=numStr;
     square.id="square_div";
     container.appendChild(square);
 }
 const squares = document.querySelectorAll("#square_div");
 squares.forEach((square)=>{
     square.addEventListener('mouseover',()=>{
-        square.style.backgroundColor='cyan';
+        square.style.backgroundColor=colorConstructor(colorLiterals);
     })
+})
+
+const btn = document.querySelector("#btn");
+btn.addEventListener('click',()=>{
+    window.location.reload();
 })
